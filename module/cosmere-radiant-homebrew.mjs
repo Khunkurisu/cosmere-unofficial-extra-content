79c41f2ab76a1b6c1a94bfaf3e6f4cc8964852d3
Khunkurisu <creative.3d@live.com>
2025-04-28 02:09:08 -0500

    fix: check for workbench module + active flag properly, then safety check for existing skills to avoid crash
    Signed-off-by: Khunkurisu <creative.3d@live.com>

--- module/cosmere-radiant-homebrew.mjs
+++ module/cosmere-radiant-homebrew.mjs
@@ -4,26 +4,29 @@ Hooks.once('init', () => {
 	globalThis.cosmereHomebrewRadiants = Object.assign(
 		game.modules.get('cosmere-radiant-homebrew')
 	);
-	if (!cosmereWorkbench) {
+	if (game.modules.get('cosmere-rpg-workbench') && game.modules.get('cosmere-rpg-workbench').active) {
+		Hooks.once('customSkillRegistry', (customSkills) => {
+			radiantSkills.forEach((skill) => {
+				if (!customSkills.find((s) => s.id === skill.id)) {
+					console.log(skill);
+					customSkills.push(skill);
+				}
+			});
+		});
+	} else {
 		console.log("Cosmere RPG Workbench not found, registering skills manually.");
 		radiantSkills.forEach((skill) => {
-			cosmereRPG.api.registerSkill(skill);
+			if (!CONFIG.COSMERE.skills[skill]) {
+				cosmereRPG.api.registerSkill(skill);
+			}
 		});
 	}
 });
 
 Hooks.once('ready', () => {
-	if (!cosmereWorkbench) {
+	if (!(game.modules.get('cosmere-rpg-workbench') && game.modules.get('cosmere-rpg-workbench').active)) {
 		radiantSkills.forEach((skill) => {
 			game.i18n.translations.COSMERE.Skill[skill.id] = skill.label;
 		});
 	}
-});
-
-Hooks.once('customSkillRegistry', (customSkills) => {
-	radiantSkills.forEach((skill) => {
-		if (!customSkills.find((s) => s.id === skill.id)) {
-			customSkills.push(skill);
-		}
-	});
 });
